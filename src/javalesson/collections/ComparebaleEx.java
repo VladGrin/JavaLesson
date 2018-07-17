b4a13588aaa54dd0aa47cb34ec5e2989f4ee1972
Volodymyr <vlad5855051@gmail.com>
2018-07-17 16:56:42 +0300

    Comparator

--- src/javalesson/collections/ComparebaleEx.java
+++ src/javalesson/collections/ComparebaleEx.java
@@ -1,11 +1,13 @@
 package javalesson.collections;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class ComparebaleEx {
     public static void main(String[] args) {
         Set<MyPerson> list = new TreeSet<>();
+        list = new TreeSet<>(new ComparePerson());
         list.add(new MyPerson(5));
         list.add(new MyPerson(10));
         list.add(new MyPerson(8));
@@ -13,7 +15,17 @@ public class ComparebaleEx {
         list.add(new MyPerson(6));
         System.out.println(list );
         list.stream().sorted().forEach(x-> System.out.println(x));
+        list.stream().forEach(x-> System.out.println(x));
 
+
+    }
+}
+
+class ComparePerson implements Comparator<MyPerson> {
+
+    @Override
+    public int compare(MyPerson o1, MyPerson o2) {
+        return o1.getAge() - o2.getAge();
     }
 }
 
@@ -38,6 +50,7 @@ class MyPerson implements Comparable<MyPerson>{
                 "age=" + age +
                 '}';
     }
+
 
     @Override
     public int compareTo(MyPerson o) {
