89396464752204315aa6a70c3ba1e961cea15fd4
Volodymyr <vlad5855051@gmail.com>
2018-07-27 12:00:27 +0300

    Patterns Proxy

---
+++ src/javalesson/patterns/structural/ProxyLesson.java
@@ -0,0 +1,26 @@
+package javalesson.patterns.structural;
+
+public class ProxyLesson {
+    public static void main(String[] args) {
+        CarI carI = new ProxyCar();
+        carI.drive();
+    }
+}
+interface CarI{
+    void drive();
+}
+class Renault implements CarI{
+
+    @Override
+    public void drive() {
+        System.out.println("Drive Renault");
+    }
+}
+class ProxyCar implements CarI{
+    CarI carI = new Renault();
+    @Override
+    public void drive() {
+        System.out.println("Proxy code");
+        carI.drive();
+    }
+}
