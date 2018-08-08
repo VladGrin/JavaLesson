5ebe22c17ee4389adb0c1c7df96532cd11752ff3
Volodymyr <vlad5855051@gmail.com>
2018-08-08 19:15:16 +0300

    Stepic Stream Task 2

--- src/javalesson/stepic/Stream22.java
+++ src/javalesson/stepic/Stream22.java
@@ -1,7 +1,9 @@
 package javalesson.stepic;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Stream22 {
@@ -12,7 +14,14 @@ public class Stream22 {
             Stream<? extends T> stream,
             Comparator<? super T> order,
             BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<T> list = stream.sorted(order).collect(Collectors.toList());
 
-
+        if (list.isEmpty()){
+            minMaxConsumer.accept(null, null);
+        } else {
+            T min = list.get(0);
+            T max = list.get(list.size() - 1);
+            minMaxConsumer.accept(min, max);
+        }
     }
 }
