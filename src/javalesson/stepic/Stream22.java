11acb3858c184560a6eada481642e13988f45c09
Volodymyr <vlad5855051@gmail.com>
2018-08-08 18:54:15 +0300

    Stepic Stream Task 2

---
+++ src/javalesson/stepic/Stream22.java
@@ -0,0 +1,18 @@
+package javalesson.stepic;
+
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+public class Stream22 {
+    public static void main(String[] args) {
+
+    }
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+
+    }
+}
