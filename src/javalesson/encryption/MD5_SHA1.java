6a94edc7ffe620614158254209b7615daf50b73a
Volodymyr <vlad5855051@gmail.com>
2018-08-22 13:29:16 +0300

    Encription MD5, SHA-1

---
+++ src/javalesson/encryption/MD5_SHA1.java
@@ -0,0 +1,23 @@
+package javalesson.encryption;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class MD5_SHA1 {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String str = "hello world";
+        // "SHA-1", "MD5"
+
+        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
+        MessageDigest md5 = MessageDigest.getInstance("MD5");
+        byte[] bytes = md5.digest(str.getBytes());
+
+        StringBuilder builder = new StringBuilder();
+        for (byte b : bytes) {
+            builder.append(String.format("%02X ", b));
+        }
+        System.out.println(builder.toString());
+    }
+}
