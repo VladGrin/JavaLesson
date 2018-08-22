a591224129ed6aa734bf3af5c89ad6dcc879575f
Volodymyr <vlad5855051@gmail.com>
2018-08-22 15:02:45 +0300

    Run Java Script

---
+++ src/javalesson/annotation.java
@@ -0,0 +1,22 @@
+package javalesson;
+
+public class annotation {
+    public static void main(String[] args) {
+
+    }
+}
+
+@Deprecated
+class MyClass{
+    @Deprecated
+    int i;
+
+
+    MyClass(){
+
+    }
+
+    public void method(){
+
+    }
+}
--- src/javalesson/javascript/RunJavaScript.java
+++ src/javalesson/javascript/RunJavaScript.java
@@ -1,4 +1,26 @@
 package javalesson.javascript;
 
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class RunJavaScript {
+    public static void main(String[] args) throws ScriptException, NoSuchMethodException {
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+        engine.put("k", 4);
+        String code = "print('hello');" +
+                "function f(a){return a + 6;}" +
+                "var n = 1 + 2;" +
+                "n + 3 + k";
+
+        Object o = engine.eval(code);
+        System.out.println(o);
+        Object o2 = ((Invocable)engine).invokeFunction("f",1);
+        System.out.println(o2);
+        Object o3 = ((Invocable)engine).invokeFunction("f","1");
+        System.out.println(o3 );
+    }
 }
